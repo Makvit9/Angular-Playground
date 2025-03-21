0422c83222e22a3de9883f4a1868524dd937a0d2
crynged9 <crynged9@gmail.com>
2025-03-21 17:39:11 +0000

    First Lesson

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: `
+    <h1>Testing 1 2 3 ! !</h1>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Homes';
+}
